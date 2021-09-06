c22100e4282c7fb99d0d0de58113d635deaf542c
xingshou <494173807@qq.com>
2021-09-06 21:09:30 +0800

    塔

---
+++ assets/Script/Soldier/tower.ts
@@ -0,0 +1,24 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import SoldiersParent from "../Other/SoldiersParent";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends SoldiersParent {
+
+    start () {
+
+    }
+
+    update(){
+
+    }
+
+    // update (dt) {}
+}
