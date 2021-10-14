33d26c7e81698e422c705fcdf6e2394f3bb1fbf7
xingshou <494173807@qq.com>
2021-10-14 20:34:02 +0800

    逻辑

--- assets/Script/UIManager/ButtonToolMain.ts
+++ assets/Script/UIManager/ButtonToolMain.ts
@@ -23,22 +23,30 @@ export default class ButtonToolMain extends UIParent {
 
     radius: number = 100
 
+    start() {
+        this.btnItem.active = false
+    }
+
     ShowUI(fun = () => { }, data: BtnToolInfo) {
         super.ShowUI();
-        let p1 = this.btnParent.convertToNodeSpaceAR(data.pos)
-        this.btnParent.setPosition(p1)
+        this.btnParent.removeAllChildren()
+        this.scheduleOnce(() => {
+            let p1 = this.btnParent.parent.convertToNodeSpaceAR(data.pos)
+            this.btnParent.setPosition(p1)
+        }, 0.02)
 
         let len = data.btnInfo.length
         let angle = 360 / len
         for (let index = 0; index < data.btnInfo.length; index++) {
             let element = data.btnInfo[index];
             let obj = cc.instantiate(this.btnItem)
+            obj.active = true
             this.btnParent.addChild(obj)
             let spr = obj.getComponent(buttonToolItem)
             spr.resetData(element)
 
-            let x = this.radius * Math.cos(angle * index * Math.PI / 180)
-            let y = this.radius * Math.sin(angle * index * Math.PI / 180)
+            let x = this.radius * Math.cos(((angle * index) + 90) * Math.PI / 180)
+            let y = this.radius * Math.sin(((angle * index) + 90) * Math.PI / 180)
             obj.setPosition(x, y)
         }
     }
