7a39aa263429c960bb9ddd4b7b56c581da562f59
xingshou <494173807@qq.com>
2021-10-15 19:30:55 +0800

    添加物品配置表

--- assets/Script/Config/ManorConfig.ts
+++ assets/Script/Config/ManorConfig.ts
@@ -4,5 +4,12 @@ export default class ManorConfig {
 }
 
 export class BuildingInfo {
-    buildongName: string = ""   //建筑名字
+    buildingName: BuildingType = BuildingType.City   //建筑名字
+    buildingLevel: number = 1   //建筑等级
+
 }
+
+export enum BuildingType {
+    City = "City",              //游戏结束
+}
+
--- assets/Script/Ctrl/ManorCtrl.ts
+++ assets/Script/Ctrl/ManorCtrl.ts
@@ -1,3 +1,4 @@
+import { BuildingInfo, BuildingType } from "../Config/ManorConfig";
 
 const { ccclass, property } = cc._decorator;
 
@@ -5,6 +6,7 @@ const { ccclass, property } = cc._decorator;
 export default class ManorCtrl {
     private static _instance: ManorCtrl = null;
 
+    private _buildingInfoList: { [key: string]: BuildingInfo } = null
     // LIFE-CYCLE CALLBACKS:
 
     public static getInstance() {
@@ -17,5 +19,41 @@ export default class ManorCtrl {
 
     private _init() {
 
+    }
+
+    setBuildingInfo(info: BuildingInfo) {
+        this._saveLocalData(info.buildingName, info)
+    }
+
+    getBuildingInfo(type: BuildingType) {
+        if (!this._buildingInfoList[type]) {
+            let data: BuildingInfo = this._getLocalData(type)
+            if (!data) {
+                data = new BuildingInfo()
+                data.buildingName = type
+                data.buildingLevel = 1
+            }
+            this._buildingInfoList[type] = data
+        }
+        return this._buildingInfoList[type]
+    }
+
+    getBuitdingUpLevel(type: BuildingType) {
+        let info = this.getBuildingInfo(type)
+        return info.buildingLevel * 100
+    }
+
+    private _saveLocalData(path: string, data: any) {
+        let localData = JSON.stringify(data);
+        cc.sys.localStorage.setItem(path, localData);
+    }
+
+    private _getLocalData(path: string): any {
+        let localData = cc.sys.localStorage.getItem(path);
+        if (localData) {
+            let nowData = JSON.parse(localData);
+            return nowData
+        }
+        return null
     }
 }
